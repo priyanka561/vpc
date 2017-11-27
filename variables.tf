2c0727677def3f45fc456b2a1eba879f9e41832d
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-11-27 11:49:31 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-    default = "us-east-1"
-}
-variable "accesskey"{
-        default = "AKIAIORCL4WU3APF54QQ"
-}
-variable "secretkey"{
-        default = "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"
-}
