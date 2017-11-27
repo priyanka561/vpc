c953edc3a3fc98c39aa62ec2baab3548a847c6cb
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-11-27 14:41:06 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -7,9 +7,3 @@ variable "aws_access_key" {
 variable "aws_secret_key" {
     default = "fsU09qQAXDM2CsyhY3rpt9x0sL6YLlM60N6X+m6E"
     }
-variable "vpc_id"{
-	default = "vpc-7be58003"
-}
-variable "subnet_id"{
-        default = "subnet-e517cbca"
-}
