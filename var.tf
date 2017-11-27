5ec7ab4ddab98f97a9fcc95d446463ddcfb468c7
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-11-27 12:17:03 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,3 +1,9 @@
 variable "aws_region" {
     default = "us-east-1"
 }
+variable "Access_key" {
+    default = "AKIAIORCL4WU3APF54QQ"
+    }
+variable "secretkey" {
+    default = "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"
+    }
