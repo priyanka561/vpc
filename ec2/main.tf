566ef72df6a000a556e0b7356890e41fd95201cf
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-12-04 16:00:27 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "ec2-terraform" {
   ami = "ami-55ef662f"
   instance_type = "t2.micro"
-  aws_vpc_id = "${var.vpc-terraform_id}"
+  aws_vpc = "${var.vpc-terraform_id}"
   subnet_id = "${var.public_subnet_id}"
   associate_public_ip_address = "true"
     vpc_security_group_ids = ["${aws_security_group.security-terraform.id}"]
