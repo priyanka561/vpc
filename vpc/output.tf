e35a2f7079caf864becbe124c2dd1c0a122f503b
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-12-04 15:45:50 +0530

    Update output.tf

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,6 +1,6 @@
 output "vpc-id"{
-  value = "${aws_vpc.vpc-terraform.id}"
+  value = "${aws_vpc.vpc-terraform_id}"
 }
 output "subnet_id" {
-   value = "${aws_subnet.public_subnet.id}"
+   value = "${aws_subnet.public_subnet_id}"
   }
