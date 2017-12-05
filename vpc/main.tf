ee7ec405827325c60f52e4d46e830d36f431b782
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-12-05 17:50:13 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 resource "aws_subnet" "private_1_subnet" {
-  vpc_id                  = "${aws_vpc.vpc-terraform.id}"
+  vpc_id                  = "${aws_vpc.vpc_terraform.id}"
   cidr_block              = "10.0.2.0/24"
   availability_zone = "us-east-1a"
   tags = {
