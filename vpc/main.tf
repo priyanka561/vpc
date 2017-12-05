694a2ae6af02fa4183229576364dd157c0589f1c
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-12-05 17:47:30 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
  }
-resource "aws_vpc" "vpc-terraform" {
+resource "aws_vpc" "vpc_terraform" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
