2d9e58010e9a3ac90059b7fad68f67d2e66514a5
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-12-06 15:02:50 +0530

    Update output.tf

--- ec2/output.tf
+++ ec2/output.tf
@@ -2,8 +2,8 @@ output "ec2"{
   value = "${aws_instance.ec2-terraform.id}"
  }
 output "public_ip"{
-  value = "${aws_instance.public_ip}"
+  value = "${aws_instance.ip.public_ip}"
   }
 output "private_ip"{
-  value = "${aws_instance.private_ip}"
+  value = "${aws_instance.ip.private_ip}"
   }
