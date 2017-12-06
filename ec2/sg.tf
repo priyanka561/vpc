b3649ecef831599e8530fbc6e39746bd54b20c05
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-12-06 14:05:21 +0530

    Update sg.tf

--- ec2/sg.tf
+++ ec2/sg.tf
@@ -33,6 +33,6 @@ resource "aws_security_group" "sec-tf" {
     from_port = 0
      to_port = 0
      protocol = "-1"
-     cidr_blocks = ["202.65.158.154/32"]
+     cidr_blocks = ["0.0.0.0/0"]
    }
  }
