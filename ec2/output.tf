46981172060073d05a3384d022cd8b34b0ea0914
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-12-06 14:48:24 +0530

    Update output.tf

--- ec2/output.tf
+++ ec2/output.tf
@@ -2,5 +2,5 @@ output "ec2"{
   value = "${aws_instance.ec2-terraform.id}"
  }
 output "public_ip"{
-  value = "${aws_instance.associate_public_ip_address}"
+  value = "${aws_instance.associate_public_ip_address.id}"
   }
