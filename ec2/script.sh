8b0d057d16bf84ababd1e0cdf33810b2dca8e7b3
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-12-07 16:02:17 +0530

    Update script.sh

--- ec2/script.sh
+++ ec2/script.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-sudo yum update -y
-#!/bin/bash
-# Install the chef client and configure chef-solo
-	wget https://packages.chef.io/files/stable/chefdk/1.1.16/ubuntu/16.04/chefdk_1.1.16-1_amd64.deb  &&
-	dpkg -i chefdk_1.1.16-1_amd64.deb &&
-	git clone https://github.com/priyanka561/jenkins.git
+sudo yum update -y &&
+wget https://packages.chef.io/files/stable/chefdk/1.1.16/ubuntu/16.04/chefdk_1.1.16-1_amd64.deb  &&
+dpkg -i chefdk_1.1.16-1_amd64.deb &&
+git clone https://github.com/priyanka561/jenkins.git
 {
 	"run_list" : [
 		"recipe[jenkins]"
