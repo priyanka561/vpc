a81302b3dd17dffaef4d96352f48724c5057e008
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-12-07 16:24:13 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "ec2-terraform" {
   subnet_id = "${var.public_subnet_id}"
   associate_public_ip_address = "true"
   vpc_security_group_ids = ["${aws_security_group.sc-grp.id}"]
-  user_data = "${file("script.sh")}"
+  user_data = "${data.template_file.script.rendered}"
    tags {
          Name = "ec2"
          Owner = "priyanka.elluri"
