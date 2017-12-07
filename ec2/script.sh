88b893383664bff6384c5d3901eaa4c0c30ac033
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-12-07 16:15:35 +0530

    Update script.sh

--- ec2/script.sh
+++ ec2/script.sh
@@ -2,11 +2,15 @@
 sudo yum update -y &&
 wget https://packages.chef.io/files/stable/chefdk/1.1.16/ubuntu/16.04/chefdk_1.1.16-1_amd64.deb  &&
 dpkg -i chefdk_1.1.16-1_amd64.deb &&
-git clone https://github.com/priyanka561/jenkins.git
+mkdir /etc/chef &&
+	mkdir /tmp/cookbooks &&
+	echo 'cookbook_path "/tmp/cookbooks"' > /etc/chef/solo.rb &&
+git clone https://github.com/priyanka561/jenkins.git /tmp/cookbooks/chef-dnow &&
+cat <<EOF > /etc/chef/attr.json &&
 {
 	"run_list" : [
 		"recipe[jenkins]"
 	]
 }
-sudo chef-client --local-mode --runlist 'recipe[jenkins]'
-
+EOF
+	chef-solo -j /etc/chef/attr.json 
