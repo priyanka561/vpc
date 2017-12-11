0331b6e356f8013271ab400798616f2d67ab269a
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-12-11 13:58:53 +0530

    Delete output.tf

--- ec2/output.tf
+++
@@ -1,5 +0,0 @@
-output "ec2"{
-  value = "${aws_instance.ec2-terraform.id}"
- }
-output "private_ip" { value = "${aws_instance.ec2-terraform.private_ip}" }
-output "public_ip"  { value = "${aws_instance.ec2-terraform.public_ip}" }
