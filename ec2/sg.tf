a1a927d189f93a88da2a0ec6e647c05f53741b0c
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-12-12 16:18:16 +0530

    Create sg.tf

---
+++ ec2/sg.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "jen_sec" {
+   name = "jen_sec"
+   tags {
+         Name = "jen_sec"
+   }
+   description = "ONLY HTTP CONNECTION INBOUD"
+   vpc_id      = "${var.vpc_id}"
+   ingress {
+        from_port = 80
+         to_port = 80
+         protocol = "TCP"
+         cidr_blocks = ["202.65.158.154/32"]
+   }
+   ingress {
+        from_port = 80
+         to_port = 80
+         protocol = "TCP"
+         cidr_blocks = ["10.0.0.0/16"]
+   }
+   ingress {
+     from_port   = "22"
+     to_port     = "22"
+    protocol    = "TCP"
+     cidr_blocks = ["202.65.158.154/32"]
+   }
+   ingress {
+     from_port   = "80"
+     to_port     = "80"
+    protocol    = "TCP"
+     cidr_blocks = ["54.161.67.14/32"]
+   }
+   egress {
+    from_port = 0
+     to_port = 0
+     protocol = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+ }
